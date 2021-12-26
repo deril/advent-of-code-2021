3c40b11e1d87cfbcc622f4993bb282c075515ebf
Dmytro Bihniak <bignyak@bigmir.net>
2021-12-26 14:20:51 +0200

    Use scanf for parsing input

--- nim/day02/main.nim
+++ nim/day02/main.nim
@@ -1,38 +1,38 @@
-import strutils
+import strutils, strscans
 
 proc part1*(input: seq[string]): int =
     ## [x, y]
-    var coordinates: array[2, int] = [0, 0]
+    var 
+        coordinates: array[2, int] = [0, 0]
+        action: string
+        num: int
     for command in input:
-        let 
-            commandArray = command.split()
-            action = commandArray[0]
-            num = parseInt commandArray[1]
-        case action
-        of "forward":
-            coordinates[0] += num
-        of "down":
-            coordinates[1] += num
-        of "up":
-            coordinates[1] -= num
+        if command.scanf("$w $i", action, num):
+            case action
+            of "forward":
+                coordinates[0] += num
+            of "down":
+                coordinates[1] += num
+            of "up":
+                coordinates[1] -= num
     coordinates[0] * coordinates[1]
 
 proc part2*(input: seq[string]): int =
     ## [x, y, aim]
-    var coordinates: array[3, int] = [0, 0, 0]
+    var 
+        coordinates: array[3, int] = [0, 0, 0]
+        action: string
+        num: int
     for command in input:
-        let
-            commandArray = command.split()
-            action = commandArray[0]
-            num = parseInt commandArray[1]
-        case action
-        of "forward":
-            coordinates[0] += num
-            coordinates[1] += coordinates[2] * num
-        of "down":
-            coordinates[2] += num
-        of "up":
-            coordinates[2] -= num
+        if command.scanf("$w $i", action, num):
+            case action
+            of "forward":
+                coordinates[0] += num
+                coordinates[1] += coordinates[2] * num
+            of "down":
+                coordinates[2] += num
+            of "up":
+                coordinates[2] -= num
     coordinates[0] * coordinates[1]
 
 when isMainModule:
